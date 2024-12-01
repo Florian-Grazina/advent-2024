b4d8d3c8c4fbb63a7509701a800ad96cda7b0d2f
Florian Grazina <Floriangrazina@gmail.com>
2024-12-01 13:53:49 +0100

    part 1

--- 01/Program.cs
+++ 01/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int result = 0;
+
+            var input = File.ReadAllLines("../../../input.txt");
+
+            List<int> list1 = [];
+            List<int> list2 = [];
+
+            foreach(var item in input)
+            {
+                var split = item.Split("   ");
+
+                list1.Add(int.Parse(split[0]));
+                list2.Add(int.Parse(split[1]));
+            }
+
+            list1.Sort();
+            list2.Sort();
+
+            for(int i = 0; i < list1.Count; i++)
+            {
+                int delta = Math.Abs(list1[i] - list2[i]);
+
+                result += delta;
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
