bb3a487a06f4215ad2abd36de1347aa37436e550
Florian Grazina <floriangrazina@gmail.com>
2024-12-19 17:15:14 +0100

    part 1

---
+++ 19/Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _19
+{
+    internal class Designer
+    {
+        public HashSet<string> Patterns = [];
+        public Dictionary<string, bool> DoableDesigns = [];
+
+        public Stack<string> Designs = [];
+        public int Score = 0;
+
+        public Designer(List<Towel> towels, string[] designs)
+        {
+            towels.ForEach(t => Patterns.Add(t.Pattern));
+            foreach (var item in designs)
+                Designs.Push(item);
+        }
+
+        public int GetNumberOfDesigns()
+        {
+            while (Designs.Count > 0)
+            {
+                Console.WriteLine($"Designs to check {Designs.Count}");
+                string design = Designs.Pop();
+                List<string> availableDesigns = [design];
+                bool isDesignDoable = false;
+
+                while (true)
+                {
+                    List<string> newDesigns = [];
+
+                    foreach (var designSub in availableDesigns)
+                    {
+                        isDesignDoable = CheckDesign(newDesigns, designSub);
+
+                        if (isDesignDoable)
+                        {
+                            Score++;
+                            DoableDesigns.TryAdd(design, true);
+                        }
+                    }
+                    if (newDesigns.Count == 0)
+                    {
+                        DoableDesigns.TryAdd(design, false);
+                        break;
+                    }
+                    availableDesigns = newDesigns;
+                }
+            }
+
+            return Score;
+        }
+
+        private bool CheckDesign(List<string> matchPatterns, string design)
+        {
+            //if(DoableDesigns.TryGetValue(design, out bool isDoable))
+            //    return isDoable;
+
+            bool toReturn = false;
+
+            foreach (var pattern in Patterns)
+            {
+                if (design.StartsWith(pattern))
+                    matchPatterns.Add(design.Substring(pattern.Length));
+             
+                if (design == pattern)
+                    toReturn = true;
+            }
+
+            return toReturn;
+        }
+    }
+}
--- 19/Program.cs
+++ 19/Program.cs
@@ -1,1 +1,13 @@
-var input = File.ReadAllLines("../../../input.txt");
+using _19;
+
+var input = File.ReadAllText("../../../input.txt");
+
+string[] towelsData = input.Split("\r\n\r\n")[0].Split(", ");
+string[] designData = input.Split("\r\n\r\n")[1].Split("\r\n");
+
+List<Towel> towels = [];
+foreach (var item in towelsData)
+    towels.Add(new(item));
+
+Designer designer = new(towels, designData);
+Console.WriteLine(designer.GetNumberOfDesigns()); 
