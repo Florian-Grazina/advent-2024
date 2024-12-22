a1e2cfb3646ebbdb608f88f95e2fb9fb4f78a78a
Florian Grazina <Floriangrazina@gmail.com>
2024-12-22 11:28:56 +0100

    init

--- 22/Program.cs
+++ 22/Program.cs
@@ -1,1 +1,15 @@
+using _22;
+
 var input = File.ReadAllLines("../../../input.txt");
+
+SecretCalculator calculator = new();
+uint result = 0;
+
+foreach(string data in input)
+{
+    result += calculator.GetSecretNumber(uint.Parse(data), 2000);
+}
+
+Console.WriteLine(result);
+
+// 869068837 low
---
+++ 22/SecretCalculator.cs
@@ -0,0 +1,56 @@
+﻿
+namespace _22
+{
+    internal class SecretCalculator
+    {
+        public uint GetSecretNumber(uint startingNumber, int numberOfDays)
+        {
+            uint secretNumber = startingNumber;
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                secretNumber = Step1(secretNumber);
+                secretNumber = Step2(secretNumber);
+                secretNumber = Step3(secretNumber);
+            }
+
+            Console.WriteLine(secretNumber);
+            return secretNumber;
+        }
+
+        private uint Step1(uint secretNumber)
+        {
+            uint result = secretNumber * 64;
+            secretNumber = Mix(secretNumber, result);
+            secretNumber = Prune(secretNumber);
+            return secretNumber;
+        }
+
+        private uint Step2(uint secretNumber)
+        {
+            uint result = (uint)Math.Floor(secretNumber / 32d);
+            secretNumber = Mix(secretNumber, result);
+            secretNumber = Prune(secretNumber);
+            return secretNumber;
+        }
+
+        private uint Step3(uint secretNumber)
+        {
+            uint result = secretNumber * 2048;
+            secretNumber = Mix(secretNumber, result);
+            secretNumber = Prune(secretNumber);
+            return secretNumber;
+        }
+
+        private uint Mix(uint secretNumber, uint number)
+        {
+            var ok = secretNumber ^ number;
+            return ok;
+        }
+
+        private uint Prune(uint secretNumber)
+        {
+            uint ok = secretNumber % 16777216;
+            return ok;
+        }
+    }
+}
